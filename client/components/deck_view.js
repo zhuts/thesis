67689e59435d8da53e95371d84ee91c45221763c
Raymond <rayto510@gmail.com>
2016-06-16 15:47:45 -0700

    added return statements

--- client/components/deck_view.js
+++ client/components/deck_view.js
@@ -1,38 +1,48 @@
 let deckView = React.createClass({
   render: function() {
-    <div className="fullscreen">
-      <searchResults />
-      <swipeCard />
-      <leftSwipeBtn />
-      <rightSwipeBtn />
-    </div>
+    return (
+      <div className="fullscreen">
+        <searchResults />
+        <swipeCard />
+        <leftSwipeBtn />
+        <rightSwipeBtn />
+      </div>
+    )
   }
 });
 
 let searchResults = React.createClass({
   render: function() {
-    <h2></h2>
+    return (
+      <h2></h2>
+    )
   }
 });
 
 let swipeCard = React.createClass({
   render: function() {
-    <div>
-      <cardTitle />
-      <cardPic />
-    </div>
+    return (
+      <div>
+        <cardTitle />
+        <cardPic />
+      </div>
+    )
   }
 });
 
 let cardTitle = React.createClass({
   render: function() {
-    <h2></h2>
+    return (
+      <h2></h2>
+    )
   }
 });
 
 let cardPic = React.createClass({
   render: function() {
-    <div className="card-img"></div>
+    return (
+      <div className="card-img"></div>
+    )
   }
 });
 
