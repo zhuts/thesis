901acf58bddbb4225a96041df8f3e74fce0da83b
zhuts <zhutsell@gmail.com>
2016-06-25 14:13:04 -0700

    fixes configure store

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, compose } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 
-export default configure = () => {
+export default function configure (){
   const store = createStore(
     rootReducer, compose(
       window.devToolsExtension ? window.devToolsExtension() : f => f
