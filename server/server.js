fb61158003c17226bc47b1cdc7473a662a4e7b3c
Timothy Woo <timwoo24@gmail.com>
2016-07-04 22:53:50 -0700

    close #139 update server file

--- server/server.js
+++ server/server.js
@@ -15,15 +15,10 @@ var yelpRouter = require('./routes/yelpRouter');
 var deckRouter = require('./routes/deckRouter');
 var userRouter = require('./routes/userRouter');
 var s3Router = require('./routes/s3Router');
-// var jwtCheck = jwt({
-//   secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'), // new Buffer(auth0.clientSecret),
-//   audience: process.env.AUTH0_CLIENT_ID// auth0.clientID
-// })
 var jwtCheck = jwt({
-  secret: new Buffer('1xEqRPNQDnR3EACyvfRtjTpFulhmZnQ47XvyeNBpU3GpQo7RUE5aGk97m1TsCmX4', 'base64'),
-  audience: 'zpT4RjC4avB8RuI3wEd13nNYsnPDrBrS'
-});
-console.log(process.env.AUTH0_CLIENT_SECRET, process.env.AUTH0_CLIENT_ID);
+  secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
+  audience: process.env.AUTH0_CLIENT_ID
+})
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
