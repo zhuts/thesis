4389e92c3554902a9e65daea940d5faecb00f42c
Raymond To <rayto510@gmail.com>
2016-07-06 19:49:15 -0700

    close #175 add yelp logo on decks generated from yelp search

--- client/components/viewCard.js
+++ client/components/viewCard.js
@@ -10,6 +10,9 @@ import helpers from '../util/helpers';
 import _ from 'underscore';
 
 export default class Card extends Component {
+  componentDidMount() {
+    console.log(this.props);
+  }
   
   setCurrentDeck(deck) {
     const { navigator, setCurrentViewDeck, user_id, created } = this.props;
@@ -30,7 +33,7 @@ export default class Card extends Component {
   }
     
   render() {
-    const { deck, user_id, created } = this.props;
+    const { deck, user_id, created, type } = this.props;
     let swiped = _.find(deck.shared, (user) => {
       return user.user_id === user_id;
     });
@@ -39,10 +42,14 @@ export default class Card extends Component {
         <TouchableOpacity
           onPress={() => { this.setCurrentDeck(deck) }}
         >
-          <Image 
+        { type === 'yelp' ? <Image 
             style={created === 'shared' && swiped.swiped ? styles.swiped : styles.img}
-            source={{uri: deck.deck[0].image_url}} />  
-          <Text>{deck.name}</Text>
+            source={{uri: deck.deck[0].image_url}}>
+            <Image style={styles.yelpSmall} source={require('../assets/yelp-logo-small.png')} /> 
+          </Image> : <Image 
+            style={created === 'shared' && swiped.swiped ? styles.swiped : styles.img}
+            source={{uri: deck.deck[0].image_url}}/> }
+          <Text>{deck.name}</Text> 
           <Text>
             { created === 'user' ? 'Click to see Results' : 
               swiped.swiped ? 'You voted! Click to see results' : 'Click to Vote!'
@@ -75,5 +82,11 @@ const styles = StyleSheet.create({
   },
   yelp: {
     height: 160,
+  },
+  yelpSmall: {
+    width: 100,                                                                                                                                                                                                   
+    height: 50,                                                                                                                                                                                                  
+    top: 10, 
+    left: 10,                                                                                                                                                                                                    
   }
 })
